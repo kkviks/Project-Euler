0ec5d47335021f1c6c0d6ae779ac1a7c0e76655e
kkviks <kkviks@gmail.com>
2021-07-16 23:32:51 +0530

    Add Problem 4

---
+++ 4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPelindrome(int n)
+{
+    string s = to_string(n);
+    int i = 0, j = s.size() - 1;
+    while (i < j)
+    {
+        if (s[i] != s[j])
+            return false;
+        i++, j--;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int ans = -1;
+    for (int i = 999; i >= 100; i--)
+    {
+        for (int j = 999; j >= i; j--)
+        {
+            if (isPelindrome(i * j))
+            {
+                ans = max(ans, i * j);
+            }
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
